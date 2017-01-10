6a4533651ed529629bc0570760b60fd6e7f22330
Esau Silva <esausilva85@gmail.com>
2017-01-10 06:25:12 -0600

    removed GA from index.html and inserting w/react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import MarkdownEditor from './components/MarkdownEditor';
 
+// Remove next two lines after cloning ====
+import {GoogleAnalytics} from './GoogleAnalytics';
+GoogleAnalytics();
+// ========================================
+
 ReactDOM.render(
   <MarkdownEditor />,
   document.getElementById('root')
